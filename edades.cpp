b63354fd43b5f529a13a087bddbbde273af4a1e8
UP2101612 <up210612@alumnos.upa.edu.mx>
2022-09-23 12:36:27 -0500

    edades

---
+++ edades.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main ( )
+{
+    int edad;
+    printf ("¿Cuántos años tiene? ");
+    scanf ("%d",&edad);
+
+    if (edad>= 1 && edad <=150)
+    {
+        if (edad >=1 && edad <= 30)
+        {
+            printf ("Primera edad");
+
+        } else 
+                if (edad>= 31 && edad <=60)
+                {
+                    printf ("Segunda edad");
+
+                } else 
+                        if (edad>= 61 && edad <=90)
+                        {   
+                            printf ("tercera edad");
+
+                        } else 
+                                printf ("Horas extras");
+
+        
+    } else printf ("Edad ingresada no válida");
+
+      
+
+
+
+}
